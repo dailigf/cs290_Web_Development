b5a51dd44273bb29a0d347cd07af38a76b69ce2a
dailigf <dailigf@oregonstate.edu>
2020-06-28 07:52:10 +0900

    activity2 completed

---
+++ activity2/activity2.js
@@ -0,0 +1,14 @@
+/*******************************************************************************************************
+ * Name: Francis C. Dailig                                                                             *
+ * Desc: Activity 2 Assignment                                                                         *
+ * Date: 6 July 2020                                                                                   *
+ * ****************************************************************************************************/
+
+console.log("Lets make sure JavaScript is working.");
+
+var name = "Francis"; //Replace this with your first name
+console.log("The unicode characters of your name are:")
+for (var i = 0; i < name.length; i++){
+	console.log(name.charCodeAt(i));
+}
+console.log("Copy and paste these values for activity credit.");
